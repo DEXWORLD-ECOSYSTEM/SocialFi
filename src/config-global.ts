400f7dee30ffe94457e571433f3965dbee233782
associaasppibra <associaasppibra@gmail.com>
2026-01-27 00:23:32 +0000

    fix: sync api environment variables and update branding

---
+++ src/config-global.ts
@@ -0,0 +1,39 @@
+'use client';
+
+import { paths } from 'src/routes/paths';
+import packageJson from '../package.json';
+
+// ----------------------------------------------------------------------
+
+export type ConfigValue = {
+  appName: string;
+  appVersion: string;
+  serverUrl: string;
+  assetsDir: string;
+  isStaticExport: boolean;
+  auth: {
+    method: 'jwt';
+    skip: boolean;
+    redirectPath: string;
+  };
+};
+
+// ----------------------------------------------------------------------
+
+export const CONFIG: ConfigValue = {
+  appName: 'DEX World', // Atualizado para o nome do seu ecossistema
+  appVersion: packageJson.version,
+  // Sincronizado com a chave que você salvou na Vercel:
+  serverUrl: process.env.NEXT_PUBLIC_HOST_API ?? '', 
+  assetsDir: process.env.NEXT_PUBLIC_ASSETS_DIR ?? '',
+  isStaticExport: JSON.parse(process.env.BUILD_STATIC_EXPORT ?? 'false'),
+  /**
+   * Auth
+   * @method jwt
+   */
+  auth: {
+    method: 'jwt',
+    skip: false,
+    redirectPath: paths.dashboard.root,
+  },
+};
